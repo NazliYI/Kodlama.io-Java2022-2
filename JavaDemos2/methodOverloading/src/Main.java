f1f8c5a6e1d59df0e510620c0011f4a307b73e98
Nazlı Yıldırım <114247308+NazliYI@users.noreply.github.com>
2022-09-26 23:19:29 +0300

    Add files via upload

---
+++ JavaDemos2/methodOverloading/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        DortIslem dotrIslem = new DortIslem();
+        System.out.println(dotrIslem.topla(2,3));
+        dotrIslem.topla(2,3,5);
+    }
+}
